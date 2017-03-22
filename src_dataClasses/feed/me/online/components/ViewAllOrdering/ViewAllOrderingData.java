11dbdc00a3654f2b080a9cb2aca7443836433bde
Bui Cong Huy Cuong <huycuongbuicong@gmail.com>
2017-03-22 11:35:48 +0700

    [Cuong] delete setting and class

---
+++ src_dataClasses/feed/me/online/components/ViewAllOrdering/ViewAllOrderingData.java
@@ -0,0 +1,12 @@
+package feed.me.online.components.ViewAllOrdering;
+
+/**
+ */
+@SuppressWarnings("all")
+@javax.annotation.Generated(comments="This is the java file of the ivy data class ViewAllOrderingData", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
+public class ViewAllOrderingData extends ch.ivyteam.ivy.scripting.objects.CompositeObject
+{
+  /** SerialVersionUID */
+  private static final long serialVersionUID = 0L;
+
+}
---
+++ src_dataClasses/feed/me/online/form/ViewAllOrderingForm/ViewAllOrderingFormData.java
@@ -0,0 +1,12 @@
+package feed.me.online.form.ViewAllOrderingForm;
+
+/**
+ */
+@SuppressWarnings("all")
+@javax.annotation.Generated(comments="This is the java file of the ivy data class ViewAllOrderingFormData", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
+public class ViewAllOrderingFormData extends ch.ivyteam.ivy.scripting.objects.CompositeObject
+{
+  /** SerialVersionUID */
+  private static final long serialVersionUID = 0L;
+
+}
