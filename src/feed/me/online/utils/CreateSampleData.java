62af2b66a450827381bcf9d1ad92cec8fdce910d
HC-PC\HC <huycuongbuicong@gmail.com>
2017-04-10 23:40:01 +0700

    sample data

--- src/feed/me/online/utils/CreateSampleData.java
+++ src/feed/me/online/utils/CreateSampleData.java
@@ -18,9 +18,11 @@ public class CreateSampleData {
 	SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 	 Date date1 = new Date();
 	 Date date2 = new Date(0);
+	 Date date3 = new Date(0);
 	try {
 		date1 = sdf.parse("02-03-2017");
-		date2 = sdf.parse("10-03-2017");
+		date2 = sdf.parse("30-04-2017");
+		date3 = sdf.parse("03-01-2017");
 	} catch (ParseException e) {
 		
 		e.printStackTrace();
@@ -31,6 +33,9 @@ public class CreateSampleData {
 	
 	Restaurant dominoRestaurant = new Restaurant();
 	dominoRestaurant.setName("Domino");
+	
+	Restaurant bugerKingRestaurant = new Restaurant();
+	bugerKingRestaurant.setName("BugerKing Restaurant");
 	 
 	 OrderingTicket orderingTicket1 = new OrderingTicket();
 	 orderingTicket1.setCreatedDate(date1);
@@ -42,8 +47,14 @@ public class CreateSampleData {
 	 orderingTicket2.setFoodItems(createListOfFoodItem2());
 	 orderingTicket2.setRestaurant(dominoRestaurant);
 	 
+	 OrderingTicket orderingTicket3 = new OrderingTicket();
+	 orderingTicket3.setCreatedDate(date3);
+	 orderingTicket3.setFoodItems(createListOfFoodItem3());
+	 orderingTicket3.setRestaurant(bugerKingRestaurant);
+	 
 	 orderingTickets.add(orderingTicket1);
 	 orderingTickets.add(orderingTicket2);
+	 orderingTickets.add(orderingTicket3);
 	 return orderingTickets;
  }
 
@@ -74,6 +85,22 @@ public static List<FoodItem> createListOfFoodItem2(){
 	FoodItem foodItem2 = new FoodItem();
 	foodItem2.setName("Banana");
 	foodItem2.setPrice(40f);
+	
+	fooditems.add(foodItem1);
+	fooditems.add(foodItem2);
+	return fooditems;
+	} 
+
+public static List<FoodItem> createListOfFoodItem3(){
+	List<FoodItem> fooditems = new ArrayList<>();
+	
+	FoodItem foodItem1 = new FoodItem();
+	foodItem1.setName("Jack Fruit");
+	foodItem1.setPrice(86f);
+	
+	FoodItem foodItem2 = new FoodItem();
+	foodItem2.setName("Tomato");
+	foodItem2.setPrice(45f);
 	
 	fooditems.add(foodItem1);
 	fooditems.add(foodItem2);
